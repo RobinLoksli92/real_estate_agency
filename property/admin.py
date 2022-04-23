899d06bb52b327ed7227c5ac9b368d934461cce5
RobinLoksli92 <gleb11tiun@mail.ru>
2022-04-23 14:13:03 +0200

    setup admin UI for likes.

--- property/admin.py
+++ property/admin.py
@@ -8,9 +8,10 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    raw_id_fields = ['liked_by']
 
 class DislikeAdmin(admin.ModelAdmin):
-     raw_id_fields = ["disliked_flat"]
+    raw_id_fields = ['disliked_flat']
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Dislike, DislikeAdmin)
